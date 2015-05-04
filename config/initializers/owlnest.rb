8848c8bd45948a96d6468f47721bc388f2c1cf6a
Andreas Knöfple <andreas.knoepfle@gmail.com>
2015-05-04 17:26:19 +0200

    port specified as int

--- config/initializers/owlnest.rb
+++ config/initializers/owlnest.rb
@@ -1,1 +1,1 @@
-Owldiff::Client.setup ENV.fetch("OWLDIFF_HOST","localhost"), ENV.fetch("OWLDIFF_PORT","4567")
+Owldiff::Client.setup ENV.fetch("OWLDIFF_HOST","localhost"), ENV.fetch("OWLDIFF_PORT",4567)
