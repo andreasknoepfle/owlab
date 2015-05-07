133e286be55de142922296a92c4620026d7b0556
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2015-05-07 13:53:53 +0200

    bugfix

--- app/controllers/owlnest_controller.rb
+++ app/controllers/owlnest_controller.rb
@@ -1,15 +1,18 @@
 class OwlnestController < ApplicationController
   skip_before_action :authenticate_user!, only: [:diff,:empty]
   def diff
+    binding.pry
     @owldiff = Owldiff::Client.diff params[:parent_url], params[:child_url]
     @file_index = params[:file_index]
-    render :diff , layout: !request.xhr?
+    #render :diff , layout: !request.xhr?
+
+    render nothing: true
   rescue Errno::ECONNREFUSED => e
-    @error = "Connection to Owldiff Service could not be established: #{e.message}"
-    render :error, layout: !request.xhr?
+    @diff_error = "Connection to Owldiff Service could not be established: #{e.message}"
+    render :diff_error, layout: false#!request.xhr?
   rescue => e
-    @error = e.message
-    render :error, layout: !request.xhr?
+    @diff_error = e.message
+    render :diff_error , layout: false#!request.xhr?
   end
 
   def empty
