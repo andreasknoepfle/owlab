14dbf32b75390c84ff030c2648907deaa7e4788d
Andreas Knöfple <andreas.knoepfle@gmail.com>
2015-05-16 22:18:30 +0200

    bugfix

--- config/initializers/owlnest.rb
+++ config/initializers/owlnest.rb
@@ -1,3 +1,3 @@
 
 $DOCKER_CONTAINER_IP = %x(/sbin/ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}').strip if ENV["INSIDE_DOCKER"]
-Owldiff::Client.setup ENV.fetch("OVERWRITE_OWLDIFF_HOST", ENV["INSIDE_DOCKER"] ? "owldiff" : "localhost"), ENV.fetch("OVERWRITE_OWLDIFF_PORT",4567)
+Owldiff::Client.setup ENV.fetch("OVERWRITE_OWLDIFF_HOST", ENV["INSIDE_DOCKER"] ? "owldiff" : "localhost"), ENV.fetch("OVERWRITE_OWLDIFF_PORT",4567).to_i
